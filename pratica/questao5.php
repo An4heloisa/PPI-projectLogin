c1a422330fe60fe14e22d8449b57fe6c340d6603
An4heloisa <heloisa.ana@escolar.ifrn.edu.br>
2026-08-20 10:15:13 -0300

    Correct multiples checking logic in questao5.php
    Fix the loop condition and update the number assignment logic.

--- pratica/questao5.php
+++ pratica/questao5.php
@@ -1,14 +1,19 @@
 <?php 
 
-for($numero=1; $numero>=10; $numero++){
-    if($numero % 3 == 0){
+$numero = 3;
+
+    if($numero % 3 == 0 && $numero % 5 == 0){
+        echo $numero ." é múltiplo de 3 e 5";
+    }
+    else if($numero % 3 == 0){
         echo $numero ." é múltiplo de 3";
     }
     else if($numero % 5 == 0){
         echo $numero ." é múltiplo de 5";
     }
-    
-
+    else{
+        echo $numero . " não é múltiplo de nenhum dos dois";
+    }
 
 
 }
